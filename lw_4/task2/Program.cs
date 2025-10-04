92bd0cbfd259b3042ee366ee148f8802b0c84a56
Dmitrii Liashko <dmitrii.liashko>
2025-10-05 00:37:02 +0300

    4 lw c#

---
+++ lw_4/task2/Program.cs
@@ -0,0 +1,11 @@
+﻿namespace task2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Window window = new Window(500, 500, "lab4 task2");
+            window.Run(60.0);
+        }
+    }
+}
